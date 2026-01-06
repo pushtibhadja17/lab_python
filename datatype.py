86206275b79f0d054ce7e4827de26c8dc00d6799
pushtibhadja17 <pushtibhadja17@gmail.com>
2026-01-06 08:13:49 +0530

    Add files via upload

---
+++ datatype.py
@@ -0,0 +1,27 @@
+print("WELCOME TO ATMIYA UNIVERSITY")
+x= int(20)
+print(x)
+x=float(20.5)
+print(x)
+x= complex(1j)
+print(x)
+x=list["apple","banana","cherry"]
+print(x)
+x=("apple","banana","cherry")
+print(x)
+x=range(7)
+print(x)
+x={"name":"john","age":"27"}
+print(x)
+x=frozenset({"apple","banana","cherry"})
+print(x)
+x=True
+print(x)
+x=b"hello"
+print(x)
+x=bytearray(5)
+print(x)
+x=memoryview(bytes(5))
+print(x)
+x=None
+print(x)
