df92a4c219df33154bd2c45583ef9e2930b5e5f7
pushtibhadja17 <pushtibhadja17@gmail.com>
2026-01-20 08:45:06 +0530

    Add files via upload

---
+++ practicaltask.py
@@ -0,0 +1,22 @@
+"""#calculate simple interest
+p=float(input("enter principle amount:"))
+r=float(input("enter rate of interest:"))
+t=float(input("enter time(in year):"))
+simple_interest = (p*r*t)/100
+print("simple interest=",simple_interest)
+
+#find maximum of 2 numbers
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+if a>b:
+    print("maximum number is:",a)
+else:
+     print("maximum number is:",b)"""
+
+#print numbers 1 to 5
+for i in range(1,6):
+    print(i)
+
+
+
+
