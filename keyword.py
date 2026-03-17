46d7bb4c86001720b878b949a1a67b0ce840493e
pushtibhadja17 <pushtibhadja17@gmail.com>
2026-03-17 08:25:44 +0530

    Add files via upload

---
+++ keyword.py
@@ -0,0 +1,19 @@
+"""def student_info(name,age,city):
+    print("name:",name)
+    print("age:",age)
+    print("city:",city)
+student_info(age=18,city="rajkot",name="Ravi")
+
+
+def display(a,b,c):
+    print("a=",a)
+    print("b=",b)
+    print("c=",c)
+display(1,c=3,b=2)"""
+
+
+def simple_interest(p:float,r:int,t:float):
+    si=(p*r*t)/100
+    print("simple interest:",si)
+
+simple_interest(p=100000,t=2,r=1.5)
