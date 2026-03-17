5ad2ffaffaba38351db5a2384eedc72d8c2f9c44
pushtibhadja17 <pushtibhadja17@gmail.com>
2026-03-17 08:26:07 +0530

    Add files via upload

---
+++ loopstatement.py
@@ -0,0 +1,15 @@
+"""age=int(input("enter your age:"))
+if age>=18:
+    print("you're eligible")
+else:
+        print("you aren't eligible")"""
+
+a=input("enter a:")
+b=input("enter b:")
+c=input("enter c:")
+if a>b:
+    print("a is big..",a)
+elif c>b:
+    print("c is big..",c)
+else:
+    print("b is big..",b)
