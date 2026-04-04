e3ea84ea3676a92018193a77f137cc009971807c
pushtibhadja17 <pushtibhadja17@gmail.com>
2026-04-04 08:17:06 +0530

    Add files via upload

---
+++ forloop.py
@@ -0,0 +1,35 @@
+"""for i in range (1,11):
+    print(i)
+
+
+
+for i in range (1,21):
+        if i%2==0:
+            print(i)
+
+
+
+for i in range(1,25):
+    if i%2==1:
+        print(i)
+
+
+
+for i in range(1,11):
+    print("5 x",i,"=",5*i)
+
+
+
+name="HELLOW WORLD!"
+for letter in name:
+    print(letter)"""
+
+
+
+
+
+
+
+
+
+
